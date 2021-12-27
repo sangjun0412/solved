f10d3a85abf98fa7cb1481019979ebc76861d42c
sangjun0412 <69152942+sangjun0412@users.noreply.github.com>
2021-12-27 16:19:44 +0900

    Update 2438.py
    print("\n")이걸 해서 띄어쓰기가 두번됨

--- 2438.py
+++ 2438.py
@@ -2,6 +2,6 @@ def solve():
     N = int(input())
     for x in range(1,N+1):
         for j in range(x):
-            print("*",end="")
-        print("\n") 
+            print("*",end='')
+        print("")
 solve()
