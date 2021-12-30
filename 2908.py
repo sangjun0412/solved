308022ba2c8d7ae0e4ffa54451f6de78a36932d1
sangjun0412 <69152942+sangjun0412@users.noreply.github.com>
2021-12-30 11:04:41 +0900

    Create 2908.py

---
+++ 2908.py
@@ -0,0 +1,19 @@
+def solve(a,b):
+    print(a,b)
+    c=""
+    d=""
+    for i in a:
+        c= i+c
+    for j in b:
+        d= j+d
+
+    c= int(c)
+    d= int(d)
+    if(c>d):
+        print(c)
+    elif(d>c):
+        print(d)
+
+a,b = map(str, input().split())
+if "0" not in a and b:
+    solve(a,b)
