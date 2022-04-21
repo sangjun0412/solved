6af838b4743734366b89a3226602471c9b59abcc
sangjun0412 <guaba0412@gmail.com>
2022-04-21 09:28:12 +0900

    Create 11650.py

---
+++ 11650.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+array = []
+
+for i in range(n):
+    [a, b] = map(int, input().split())
+    array.append([a, b])
+array.sort()
+
+for i in range(n):
+    print(array[i][0], array[i][1])
