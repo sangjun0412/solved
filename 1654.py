18f845f8558989e9fc43c9d7748382ba828249c0
sangjun0412 <guaba0412@gmail.com>
2022-05-15 22:38:17 +0900

    Update 1654.py

--- 1654.py
+++ 1654.py
@@ -9,4 +9,14 @@ for i in range(k):
 x.sort()
 start = 1
 end = x[-1]
+
+while(start <= end):
+    mid = (start + end) // 2
+    cnt = 0
+    for i in x:
+        cnt += i//mid
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
 print(end)
