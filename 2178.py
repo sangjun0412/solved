c8b16c8d56265ebbe8f02e5da982a6f9da555da0
sangjun0412 <guaba0412@gmail.com>
2022-06-12 19:31:04 +0900

    Update 2178.py

--- 2178.py
+++ 2178.py
@@ -1,8 +1,34 @@
-from re import L
-
-
+from collections import deque
 n, m = map(int, input().split())
-list1 = []
+graph = []
 for _ in range(n):
-    list1.append(int(input()))
-print(list1)
+    graph.append(list(map(int, input())))
+
+
+def bfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    que = deque()
+    que.append((x, y))
+
+    while que:
+        x, y = que.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                que.append((nx, ny))
+    return graph[n-1][m-1]
+
+
+print(bfs(0, 0))
