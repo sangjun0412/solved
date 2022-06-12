470ba6d55dfa8e5a56576e09110c0be068c2bf57
sangjun0412 <guaba0412@gmail.com>
2022-06-12 19:31:06 +0900

    Update 2606.py

--- 2606.py
+++ 2606.py
@@ -1,9 +1,28 @@
+from collections import deque
+
 n = int(input())
 m = int(input())
-graph = []
+graph = [[] for i in range(n+1)]
+visit = []
+
 
 for _ in range(m):
-    graph.append((map(int, input())))
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for i in graph:
-    print(i)
+
+def bfs(x):
+    que = deque([x])
+    visit.append(x)
+
+    while que:
+        v = que.popleft()
+        for i in graph[v]:
+            if i not in visit:
+                que.append(i)
+                visit.append(i)
+
+
+bfs(1)
+print(len(visit)-1)
