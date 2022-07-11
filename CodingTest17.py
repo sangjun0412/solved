902d7c0a7f7937299556e62149d65fece13ea16b
sangjun0412 <guaba0412@gmail.com>
2022-07-11 20:16:04 +0900

    Create CodingTest17.py

---
+++ CodingTest17.py
@@ -0,0 +1,21 @@
+import sys
+
+t = int(sys.stdin.readline())
+list1 = []
+for i in range(t):
+	list1.append(int(sys.stdin.readline()))
+
+cnt1 = 0
+cnt0 = 0
+def fibonacci(n):
+	if n == 1:
+		cnt1 += 1
+	elif n == 0:
+		cnt0 += 1
+	else:
+		return fibonacci(n-1) + fibonacci(n-2)
+
+for i in list1:
+	print(fibonacci(i))
+	
+
