98ddff052de51adce7a90b46003eb48c708ed763
sangjun0412 <guaba0412@gmail.com>
2022-07-13 23:04:34 +0900

    Update subtest.py

--- subtest.py
+++ subtest.py
@@ -1,2 +1,25 @@
-a = [[1, 2, 3, 4, 5]for _ in range(1000)]
-print(a[0][0])
+def solution(answers):
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    first_cnt = 0
+    second_cnt = 0
+    third_cnt = 0
+    length = len(answers)
+    answer = []
+
+    for i in range(length):
+        if answers[i] == first[i % 5]:
+            first_cnt += 1
+        if answers[i] == second[i % 8]:
+            second_cnt += 1
+        if answers[i] == third[i % 10]:
+            third_cnt += 1
+    if first_cnt >= second_cnt and first_cnt >= third_cnt:
+        answer.append(1)
+    if second_cnt >= first_cnt and second_cnt >= third_cnt:
+        answer.append(2)
+    if third_cnt >= first_cnt and third_cnt >= second_cnt:
+        answer.append(3)
+
+    return answer
