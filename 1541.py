fabfcd1aba2e23fb9892403301140665ca07fbfe
sangjun0412 <guaba0412@gmail.com>
2022-07-23 13:31:39 +0900

    Update 1541.py

--- 1541.py
+++ 1541.py
@@ -1,33 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-raw = str(input().rstrip())
+a = input().split('-')
 
+num = []
 
-one_number = ""
-raw_number = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
 
-for i in raw:
-    if i == '-' or i == '+':
-        raw_number.append(one_number)
-        raw_number.append(i)
-        one_number = ""
-    else:
-        print(i)
-        one_number += i
-raw_number.append(one_number)
+result = num[0]
 
+for i in range(1, len(num)):
+    result -= num[i]
 
-print(raw_number)
-
-result = 0
-
-change_number = []
-
-for i in range(len(raw_number)):
-    if i == '+':
-        result += (int(raw_number[i-1]) + int(raw_number[i+1]))
-    if i == '-':
-        change_number.append(i)
-
-print(change_number)
+print(result)
