1ea2487bd9213f3ea4a81ac82e54f93b978ddadf
sangjun0412 <guaba0412@gmail.com>
2022-07-24 21:23:32 +0900

    Update 18870.py

--- 18870.py
+++ 18870.py
@@ -1,15 +1,12 @@
 import sys
-import copy
 input = sys.stdin.readline
 
 n = int(input())
 x = list(map(int, input().split()))
 y = list(set(x))
 y.sort()
-print(x)
-print(y)
+
 dic = {y[i]: i for i in range(len(y))}
-print(dic)
 
 for i in x:
     print(dic[i], end=' ')
