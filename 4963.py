79546d1f6c2411d1e644d153ff3e66b4a6712f40
sangjun0412 <guaba0412@gmail.com>
2022-07-25 13:55:43 +0900

    Create 4963.py

---
+++ 4963.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs(x, y):
+    q = deque()
+    q.append([x, y])
+    dx = [1, -1, 0, 0, 1, 1, -1, - 1]
+    dy = [0, 0, 1, -1, 1, -1, 1, -1]
+    field[x][y] = 0
+    while q:
+        a, b = q.popleft()
+        for i in range(8):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and field[nx][ny] == 1:
+                field[nx][ny] = 0
+                q.append([nx, ny])
+
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    field = []
+    count = 0
+    for _ in range(h):
+        field.append(list(map(int, input().split())))
+
+    for i in range(h):
+        for j in range(h):
+            if field[i][j] == 1:
+                bfs(i, j)
+                count += 1
+    print(count)
