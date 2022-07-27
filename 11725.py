6259ab097df160197eb15f80fa09dc2d09222acb
sangjun0412 <guaba0412@gmail.com>
2022-07-27 15:04:27 +0900

    Create 11725.py

---
+++ 11725.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n + 1)]
+
+for i in range(n - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(n + 1):
+    graph[i].sort()
+
+visited = [0] * (n + 1)
+
+
+def dfs(node):
+    for i in graph[node]:
+        if visited[i] == 0 and i != 1:
+            visited[i] = node
+            dfs(i)
+
+
+dfs(1)
+
+for i in range(2, n + 1):
+    print(visited[i])
